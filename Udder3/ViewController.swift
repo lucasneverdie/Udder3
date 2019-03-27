b052966c445a1a655202681e80b078cf197d2d49
Lucas Huang <lucas@lucashuang.com>
2019-03-27 17:36:02 +0800

    Initial Commit

---
+++ Udder3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder3
+//
+//  Created by lucas on 2019/3/27.
+//  Copyright © 2019 Lucas Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
